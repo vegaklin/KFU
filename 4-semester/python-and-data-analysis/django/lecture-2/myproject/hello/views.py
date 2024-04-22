2d0b610b6497040d5fd6607a049e740fdb5f9c2f
vegaklin <ruslan.evn@mail.ru>
2024-04-22 14:36:20 +0300

    feat: add django sending status codes in views

--- 4-semester/python-and-data-analysis/django/lecture-2/myproject/hello/views.py
+++ 4-semester/python-and-data-analysis/django/lecture-2/myproject/hello/views.py
@@ -1,12 +1,22 @@
-from django.http import HttpResponse
+from django.http import *
 
-def products(request, productid):
-    category = request.GET.get("cat", "")
-    output = "<h2>Product № {0} Category: {1}</h2>".format(productid, category) 
-    return HttpResponse(output)
+def m304(request):
+    return HttpResponseNotModified()
 
-def users(request):
-    id = request.GET.get("id", 1)
-    name = request.GET.get("name", "Tom")
-    output = "<h2>Users</h2><h3>id: {0} namme: {1}</h3>".format(id, name)
-    return HttpResponse(output)
+def m400(request):
+    return HttpResponseBadRequest("<h2>Bad Request</h2>")
+
+def m403(request):
+    return HttpResponseForbidden("<h2>Forbidden</h2>")
+
+def m404(request):
+    return HttpResponseNotFound("<h2>Not Found&lt</h2>")
+
+def m405(request):
+    return HttpResponseNotAllowed("<h2>Method is not allowed</h2>")
+
+def m410(request):
+    return HttpResponseGone("<h2>Content is no longer here</h2>")
+
+def m500(request):
+    return HttpResponseServerError("<h2>Something is wrong</h2>")
