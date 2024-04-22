f813fb3c32d0a0330258b35df20efd94a6a76605
vegaklin <ruslan.evn@mail.ru>
2024-04-22 14:30:28 +0300

    feat: add query string parameters

--- 4-semester/python-and-data-analysis/django/lecture-2/myproject/hello/views.py
+++ 4-semester/python-and-data-analysis/django/lecture-2/myproject/hello/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("<h2>Главная</h2>")
+def products(request, productid):
+    category = request.GET.get("cat", "")
+    output = "<h2>Product № {0} Category: {1}</h2>".format(productid, category) 
+    return HttpResponse(output)
 
-def about(request):
-    return HttpResponse("<h2>О сайте</h2>")
-
-def contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
+def users(request):
+    id = request.GET.get("id", 1)
+    name = request.GET.get("name", "Tom")
+    output = "<h2>Users</h2><h3>id: {0} namme: {1}</h3>".format(id, name)
+    return HttpResponse(output)
--- 4-semester/python-and-data-analysis/django/lecture-2/myproject/myproject/urls.py
+++ 4-semester/python-and-data-analysis/django/lecture-2/myproject/myproject/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from hello import views
 
-urlpatterns = [path(r'^about/contact/', views.contact),
-               path(r'^about', views.about), 
-               path('', views.index),
+urlpatterns = [path('products/<int:productid>/', views.products),
+               path('users/', views.users), 
                ]
